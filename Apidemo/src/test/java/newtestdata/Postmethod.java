d8089551044eab429fcdffb2726ab5e723ebec2c
USER <USER@DESKTOP-J5TRUJL>
2024-03-22 11:43:24 +0530

    added

---
+++ Apidemo/src/test/java/newtestdata/Postmethod.java
@@ -0,0 +1,49 @@
+package newtestdata;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class Postmethod {
+	
+	@BeforeClass
+	public void setup() {
+		RestAssured.baseURI= "https://reqres.in/api";
+	}
+	@Test
+	public void createResource() {
+		//To declare the payload/initiate the payload
+		String requestBody= "{\r\n"
+				+ "    \"name\": \"morpheus\",\r\n"
+				+ "    \"job\": \"leader\"\r\n"
+				+ "}";
+		// we provide information about payload data type,header fields information
+		Response response= RestAssured.given()
+				.contentType(ContentType.JSON)
+				.body(requestBody)
+				// to send the request
+				.when()
+				 .post("/users")
+				// to capture the response
+				 .then()
+				  .extract().response();
+		// to print the response and status code
+		  // to print the status code
+		int statuscode = response.getStatusCode();
+		System.out.println("Status code is"+statuscode);
+		// to print the response
+		String responseBody= response.getBody().asString();
+		System.out.println("Response bOdy is"+responseBody);
+		// validations/check points for response
+		Assert.assertEquals(201,statuscode);
+		Assert.assertEquals("morpheus",response.jsonPath().getString("name"));
+		Assert.assertEquals("leader",response.jsonPath().getString("job"));
+				
+	}
+	
+
+}
